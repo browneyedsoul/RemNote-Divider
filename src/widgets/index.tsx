bf998cbdb92878ddb3de0030ec50e52bcb48b1a3
Min-ki Park <browneyedsoul@yonsei.ac.kr>
2023-02-14 13:00:41 +0900

    2nd commit

--- src/widgets/index.tsx
+++ src/widgets/index.tsx
@@ -5,52 +5,41 @@ export const DIVIDER = "divider";
 let DividerCSS: string;
 
 async function onActivate(plugin: ReactRNPlugin) {
-  await fetch("https://raw.githubusercontent.com/browneyedsoul/RemNote-Divider/main/src/snippet.css")
-    .then((response) => response.text())
-    .then((text) => {
-      DividerCSS = text;
-      console.dir("Divider Installed!");
-    })
-    .catch((error) => console.error(error));
-  await plugin.app.registerCSS("rem-tree", DividerCSS);
+  try {
+    const localResponse = await fetch("snippet.css");
+    const localCSS = await localResponse.text();
+    DividerCSS = localCSS;
+    console.dir("Divider Installed from local path!");
+  } catch (localError) {
+    console.warn(`Failed to fetch local file: ${localError}. Falling back to remote URL.`);
+    const remoteResponse = await fetch(
+      "https://raw.githubusercontent.com/browneyedsoul/RemNote-Divider/main/src/divider.css"
+    );
+    const remoteCSS = await remoteResponse.text();
+    DividerCSS = remoteCSS;
+    console.dir("Divider Installed from CDN");
+  }
+  
+  await plugin.app.registerCSS("divider", DividerCSS);
 
-  await plugin.settings.registerStringSetting({
-    id: "color",
-    title: "Text Color (hex)",
-    description: "Provide a hex color for the text",
-    defaultValue: "#ff0000",
-  });
   await plugin.settings.registerStringSetting({
     id: "height",
     title: "Line Height (px)",
     description: "Height of the Divider Area",
-    defaultValue: "10",
+    defaultValue: "",
   });
   // Each time the setting changes, re-register the css.
   plugin.track(async (reactivePlugin) => {
-    const color = await reactivePlugin.settings.getSetting("color");
+    const height = await reactivePlugin.settings.getSetting<string>("height");
+    const heightAsNumber = await Number.parseInt(height);
     await reactivePlugin.app.registerCSS(
-      "color",
+      "height",
       `
-      [data-rem-tags~="Divider"] {
-        color: ${color};
+      #hierarchy-editor .rem[data-rem-tags~="divider"] {
+        min-height: ${heightAsNumber}px;
       }
       `
     );
-  });
-  plugin.track(async (reactivePlugin) => {
-    const height = await reactivePlugin.settings.getSetting<string>("height");
-    try {
-      const heightAsNumber = Number.parseInt(height);
-      await reactivePlugin.app.registerCSS(
-        "height",
-        `
-        [data-rem-tags~="Divider"] {
-          height: ${heightAsNumber}px;
-        }
-        `
-      );
-    } catch {}
   });
   await plugin.app.registerPowerup("Divider", DIVIDER, "Rem Containing Horizontal Line", { slots: [] });
   await plugin.app.registerCommand({
